6e1bab0b3fc76e229a327615f086c8a3d6f40463
root <root@AnonymousLife>
2024-07-21 14:38:08 +0900

    Back Up

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <article>
+        <h1>
+            {{ $post->title }}
+        </h1>
+        </article>
+        <div>
+            {!! $post->body !!}
+        </div>
+    <a href="/">Go Back</a>
+</x-layout>
